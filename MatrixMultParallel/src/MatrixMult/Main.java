eafb6f3eb6ac1b2c4d7adc6cdd3b14770d746b31
floregol <florence.robert-regol@mail.mcgill.ca>
2017-10-11 18:46:16 -0400

    Plot functions

--- MatrixMultParallel/src/MatrixMult/Main.java
+++ MatrixMultParallel/src/MatrixMult/Main.java
@@ -3,45 +3,95 @@ package MatrixMult;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class Main {
-	public static int NUMBER_OF_THREADS = 2;
-	public static int MATRIX_SIZE = 1000;
+	public static int NUMBER_OF_THREADS;
+	public static int MATRIX_SIZE;
 
 	public static void main(String[] args) {
+		NUMBER_OF_THREADS = 8;
+		MATRIX_SIZE = 50;
+		// double[][] a = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+		// double[][] b = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+		//
+		// // compute sequentially
+		// long duration = getAverageSequentialTime(a, b);
+		// System.out.println("Sequential computation time : " + duration + "ms");
+		//
+		// // compute in parallel
+		// duration = getAverageParallelTime(a, b);
+		// System.out.println("Parallel computation time : " + duration + "ms");
+		plot15();
+		plot15HighScale();
+
+	}
+
+	public static void plot14() {
 		double[][] a = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
 		double[][] b = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+		// plot parallel time by num of threads
+		for (int i = 2; i < 8; i++) {
+			System.out.println(NUMBER_OF_THREADS + ";" + getAverageParallelTime(a, b));
+			NUMBER_OF_THREADS = i * 2;
+		}
 
-		// compute sequentially
-		long startTime = System.nanoTime();
-		double[][] cSequential = sequentialMultiplyMatrix(a, b);
-		long endTime = System.nanoTime();
-		long duration = (endTime - startTime) / 1000000; // divide by 1000000 to get milliseconds.
-		System.out.println("Sequential computation time : " + duration + "ms");
+	}
 
-		// compute in parallel
-		startTime = System.nanoTime();
-		double[][] cParallele = parallelMultiplyMatrix(a, b);
-		endTime = System.nanoTime();
-		duration = (endTime - startTime) / 1000000; // divide by 1000000 to get milliseconds.
-		System.out.println("Parallel computation time : " + duration + "ms");
+	public static void plot15() {
+		// plot parallel time by num of threads
+		for (int i = 0; i < 25; i++) {
+			double[][] a = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+			double[][] b = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+			
+			System.out.println(
+					MATRIX_SIZE + "   " + getAverageParallelTime(a, b) + "   " + getAverageSequentialTime(a, b));
+			MATRIX_SIZE+=50;
 
-		// Not necessary validate the result
-		System.out.println("Both same result : " + MatrixHelper.validateMatrixResult(cParallele, cSequential));
+		}
+	}
+	public static void plot15HighScale() {
+		System.out.println("High scale");
+		// plot parallel time by num of threads
+		for (int i = 0; i < 15; i++) {
+			double[][] a = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+			double[][] b = MatrixHelper.randomMatrixGenerator(MATRIX_SIZE, MATRIX_SIZE);
+			
+			System.out.println(
+					MATRIX_SIZE + "   " + getAverageParallelTime(a, b) + "   " + getAverageSequentialTime(a, b));
+			MATRIX_SIZE+=200;
+
+		}
+	}
+	public static long getAverageSequentialTime(double[][] a, double[][] b) {
+		long averageDurationMs = 0;
+		for (int i = 0; i < 3; i++) {
+			long startTime = System.nanoTime();
+			sequentialMultiplyMatrix(a, b);
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime) / 1000000; // divide by 1000000 to get milliseconds.
+			averageDurationMs+=duration;
+		}
+		return averageDurationMs / 3;
+	}
+
+	public static long getAverageParallelTime(double[][] a, double[][] b) {
+		long averageDurationMs = 0;
+		for (int i = 0; i < 3; i++) {
+			long startTime = System.nanoTime();
+			parallelMultiplyMatrix(a, b);
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime) / 1000000;// divide by 1000000 to get milliseconds.
+			averageDurationMs+=duration;
+		}
+		return averageDurationMs / 3; 
 	}
 
 	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
-
-		if (a[0].length != b.length) { // to ensure that the size of a row in A and the size of a column in B is the
-										// same. (For clarity)
-			return null;
-		}
 		double[][] result = new double[a.length][b[0].length];
-
 		for (int j = 0; j < result.length; j++) {
 			for (int i = 0; i < result[j].length; i++) {
 				double entry = 0;
-
 				for (int k = 0; k < a.length; k++) {
 					entry += b[k][i] * a[j][k];
 				}
@@ -52,26 +102,18 @@ public class Main {
 	}
 
 	public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-		if (a[0].length != b.length) { // to ensure that the size of a row in A and the size of a column in B is the
-										// same.
-			return null;
-		}
 		double[][] result = new double[a.length][b[0].length];
-		int numOfRowsPerTask = result.length / NUMBER_OF_THREADS; // divide the rows to compute between the threads, the
-																	// last thread will take the remaining rows if it's
-																	// not even
-
+		int numOfRowsPerTask = result.length / NUMBER_OF_THREADS;
 		ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
-		int index = 0;
+		int rowIndex = 0;
 		for (int i = 0; i < NUMBER_OF_THREADS - 1; i++) {
-			int startIndex = index;
-			int endIndex = index + numOfRowsPerTask;
-			index = endIndex;
+			int startIndex = rowIndex;
+			int endIndex = rowIndex + numOfRowsPerTask;
+			rowIndex = endIndex;
 			executor.execute(new RowsComputingTask(a, b, startIndex, endIndex, result));
 		}
-		executor.execute(new RowsComputingTask(a, b, index, result.length, result)); // give the last rows to the last
-																						// task
-
+		executor.execute(new RowsComputingTask(a, b, rowIndex, result.length, result)); // give the last rows to the
+																						// last task
 		// Shut down the executor
 		executor.shutdown();
 		try {
